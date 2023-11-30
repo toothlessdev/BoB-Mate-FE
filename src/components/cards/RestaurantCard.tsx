83f9ef2150247b3404f1f5f9e38f53dc1080a304
nexusbusim <ilov1112@gmail.com>
2023-12-01 00:04:54 +0900

    feat: Restaurant page 구현

--- src/components/cards/RestaurantCard.tsx
+++ src/components/cards/RestaurantCard.tsx
@@ -1,5 +1,65 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./RestaurantCard.module.scss";
+import { ICardContainer, IRestaurantCard } from "./types";
+import { faList, faMapLocation } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
-export const RestaurantCard = (): JSX.Element => {
-    return <></>;
-};
+export const RestaurantCard = {
+    Container: ({ children }: ICardContainer) => {
+        return <div className={styles.card_container}>{children}</div>;
+    },
+    Item: ({title, address, menu, id}:IRestaurantCard): JSX.Element => {
+
+        let navigate = useNavigate();
+
+        const handleClick = () => {
+            console.log(1);
+            navigate(`/restaurant/${id}`);
+        }
+
+        return (
+            <div className={styles.card} onClick={handleClick}>
+                <div className={styles.img_container}>
+                    <img src="" alt="" />
+                </div>
+
+                <div className={styles.content_container}>
+                    <div className={styles.header}>
+                        <p className={styles.title}>
+                            {title}
+                        </p>
+                        <p className={styles.address}>
+                            <FontAwesomeIcon icon={faMapLocation} className={styles.icon}/>
+                            {address}
+                        </p>
+                    </div>
+
+                    <div className={styles.menu}>
+                        <div className={styles.title}>
+                            <FontAwesomeIcon icon={faList} className={styles.icon}/>
+                            MENU
+                        </div>
+
+                        <div className={styles.item_container}>
+                            {
+                                menu.map(
+                                    (menuItem, index) => {
+                                        return (<div key={index} className={styles.item}>
+                                                    <p>
+                                                        {menuItem.menuTitle}
+                                                    </p>
+                                                    <p>
+                                                        {menuItem.price}₩
+                                                    </p>
+                                                </div>
+                                        );
+                                    }
+                                )
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- src/pages/RestaurantListPage.tsx
+++ src/pages/RestaurantListPage.tsx
@@ -1,5 +1,58 @@
+import { RestaurantCard } from "../components/cards/RestaurantCard";
+import { IRestaurantCard } from "../components/cards/types";
 import styles from "./RestaurantListPage.module.scss";
 
 export default function RestaurantListPage(): JSX.Element {
-    return <>음식점 목록 페이지</>;
+    return (
+        <main>
+            <RestaurantCard.Container>
+                {
+                    data.map((item, index) => {
+                        return <RestaurantCard.Item key={index} title={item.title} address={item.address} menu={item.menu} id={item.id}/>
+                    })
+                }
+            </RestaurantCard.Container>
+        </main>
+    );
 }
+
+const data: IRestaurantCard[] = [
+    {
+        id: 1,
+        title: 'Allison Ltd',
+        address: '1959 Thompson ForksEast Jason, GA 49037',
+        menu: [
+            {
+                menuTitle: 'Sloppy Joe Chicken Burger',
+                price: 4000
+            },
+            {
+                menuTitle: 'Grandmamas Signature Hot Chocolate',
+                price: 2800
+            },
+            {
+                menuTitle: 'Four Cheese Pizza',
+                price: 5600
+            },
+        ]
+    },
+    {
+        id: 2,
+        title: 'Allison Ltd 02',
+        address: '1959 Thompson ForksEast Jason, GA 49037',
+        menu: [
+            {
+                menuTitle: 'Sloppy Joe Chicken Burger',
+                price: 4000
+            },
+            {
+                menuTitle: 'Grandmamas Signature Hot Chocolate',
+                price: 2800
+            },
+            {
+                menuTitle: 'Four Cheese Pizza',
+                price: 5600
+            },
+        ]
+    }
+]
