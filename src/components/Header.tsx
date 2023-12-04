83ff9aaf4c3d7d80e38cc6333f58d3890d93954f
toothless <toothless042@gmail.com>
2023-12-04 19:00:18 +0900

    feat: 로그인 프로필 데이터 페칭

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,40 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUtensils } from "@fortawesome/free-solid-svg-icons";
 
 import { Button } from "./interface/Button";
+import { UserProfile } from "./cards/User";
 import styles from "./Header.module.scss";
-import { useToken } from "../hooks/useAuth";
+import { API_BASE_URL } from "../constants/api";
 
 export const Nav = (): JSX.Element => {
     const navigate = useNavigate();
+    const [userInfo, setUserInfo] = useState({
+        nickname: "",
+        email: "",
+    });
+
+    useEffect(() => {
+        if (!localStorage.getItem("token")) {
+            try {
+                const request = async () => {
+                    const response = await fetch(API_BASE_URL + `/account/profile?token=${localStorage.getItem("token")}`);
+                    if (!response.ok) throw new Error();
+                    else {
+                        const data = await response.json();
+                        setUserInfo({
+                            ...userInfo,
+                            nickname: data.nickname,
+                            email: data.email,
+                        });
+                    }
+                };
+                request();
+            } catch (err) {}
+        }
+    }, [localStorage.getItem("token")]);
 
     // const data = useToken();
     return (
@@ -21,12 +46,20 @@ export const Nav = (): JSX.Element => {
                 </div>
 
                 <div className={styles.auth}>
-                    <Button type="primary-fill" width="90px" height="45px" onClick={() => navigate("/auth/signin")}>
-                        로그인
-                    </Button>
-                    <Button type="primary-stroke" width="90px" height="45px" onClick={() => navigate("/auth/signup")}>
-                        회원가입
-                    </Button>
+                    {!localStorage.getItem("token") ? (
+                        <>
+                            <Button type="primary-fill" width="90px" height="45px" onClick={() => navigate("/auth/signin")}>
+                                로그인
+                            </Button>
+                            <Button type="primary-stroke" width="90px" height="45px" onClick={() => navigate("/auth/signup")}>
+                                회원가입
+                            </Button>
+                        </>
+                    ) : (
+                        <>
+                            <UserProfile name={userInfo.nickname}></UserProfile>
+                        </>
+                    )}
                 </div>
             </div>
         </nav>
