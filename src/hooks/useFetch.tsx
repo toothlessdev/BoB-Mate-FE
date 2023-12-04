e3386d466b1efa15ca7683395cd0c30e702be4cd
toothless <toothless042@gmail.com>
2023-12-04 21:54:24 +0900

    feat: auth fetch hook 구현

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export enum FetchStatus {
     IDLE = "IDLE",
@@ -12,6 +13,41 @@ export interface IUseFetch {
     error: any;
     data: any;
 }
+
+export const useAuthenticatedFetch = (API_URL: string): IUseFetch => {
+    const [status, setStatus] = useState<FetchStatus>(FetchStatus.IDLE);
+    const [error, setError] = useState<any>(null);
+    const [data, setData] = useState<any>(null);
+
+    const navigate = useNavigate();
+    const fetchData = useCallback(async () => {
+        try {
+            if (!localStorage.getItem("token")) {
+                throw new Error("로그인 하지 않은 사용자 접근");
+            }
+            setStatus(FetchStatus.PENDING);
+            const response = await fetch(API_URL + `?token=${localStorage.getItem("token")}`);
+            const data = await response.json();
+            setStatus(FetchStatus.SUCCESS);
+            setData(data);
+        } catch (err) {
+            setStatus(FetchStatus.FAILED);
+            setData(null);
+            setError(err);
+
+            console.log(err);
+
+            alert("로그인 해주세요");
+            navigate("/auth/signin");
+        }
+    }, [API_URL]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return { status, error, data };
+};
 
 export default function useFetch(API_URL: string): IUseFetch {
     const [status, setStatus] = useState<FetchStatus>(FetchStatus.IDLE);
